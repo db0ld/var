1b12c4b0ce1d8a7d6f1f484a9776972c742abf32
db0 <db0company@gmail.com>
2014-05-31 07:23:01 +0200

    Kevin portfolio

---
+++ Epitech/portfolio/pages/images/projets/experiences.php
@@ -0,0 +1,80 @@
+﻿
+
+Ci-dessous la liste de mes projets réalisés au sein de l'école jusqu'à aujourd'hui.
+
+<hr />
+
+<div class="projet">
+<img src="images/projets/bsq.jpg" />
+<h3>BSQ</h3>
+<h4>Date du projet :</h4>
+<p></p>
+<h4>Présentation du projet :</h4>
+<p>Il s'agit de trouver le plus grand carré possible sur 
+un plateau en évitant des obstacles.</p>
+<h4>Ce que m'a apporté ce projet :</h4>
+<p></p>
+<div class="right"><a href="sujets/sujet_bsq.txt" target="_blank">Détails du sujet</a></div>
+</div>
+
+<hr />
+
+<div class="projet">
+<img src="images/projets/exemple.jpg" />
+<h3>BSQ</h3>
+<h4>Date du projet :</h4>
+<p></p>
+<h4>Présentation du projet :</h4>
+<p>Il s'agit de trouver le plus grand carré possible sur 
+un plateau en évitant des obstacles.</p>
+<h4>Ce que m'a apporté ce projet :</h4>
+<p></p>
+<div class="right"><a href="" target="_blank">Détails du sujet</a></div>
+</div>
+
+<hr />
+
+<div class="projet">
+<img src="images/projets/exemple.jpg" />
+<h3>BSQ</h3>
+<h4>Date du projet :</h4>
+<p></p>
+<h4>Présentation du projet :</h4>
+<p>Il s'agit de trouver le plus grand carré possible sur 
+un plateau en évitant des obstacles.</p>
+<h4>Ce que m'a apporté ce projet :</h4>
+<p></p>
+<div class="right"><a href="" target="_blank">Détails du sujet</a></div>
+</div>
+
+<hr />
+
+<div class="projet">
+<img src="images/projets/exemple.jpg" />
+<h3>BSQ</h3>
+<h4>Date du projet :</h4>
+<p></p>
+<h4>Présentation du projet :</h4>
+<p>Il s'agit de trouver le plus grand carré possible sur 
+un plateau en évitant des obstacles.</p>
+<h4>Ce que m'a apporté ce projet :</h4>
+<p></p>
+<div class="right"><a href="" target="_blank">Détails du sujet</a></div>
+</div>
+
+<hr />
+
+<div class="projet">
+<img src="images/projets/exemple.jpg" />
+<h3>BSQ</h3>
+<h4>Date du projet :</h4>
+<p></p>
+<h4>Présentation du projet :</h4>
+<p>Il s'agit de trouver le plus grand carré possible sur 
+un plateau en évitant des obstacles.</p>
+<h4>Ce que m'a apporté ce projet :</h4>
+<p></p>
+<div class="right"><a href="" target="_blank">Détails du sujet</a></div>
+</div>
+
+<hr />
