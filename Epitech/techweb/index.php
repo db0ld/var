ae0ecea352cac5187d1822455dd85a8d9f3934c3
db0 <db0company@gmail.com>
2014-05-31 07:28:37 +0200

    Techweb kevin

---
+++ Epitech/techweb/index.php
@@ -0,0 +1,112 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+<head>
+  <title>TechWeb :: Solution de gestion de projet adapté Epitech</title>
+<link rel="shortcut icon" href="fav.ico">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Language" content="fr" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<meta name="DC.Language" scheme="RFC3066" content="fr" />
+<link rel="stylesheet" href="style.css" type="text/css" media="screen" title="Normal" />  
+</head>
+<body>
+<div id="header"></div>
+<div id="main">
+	<div id="left-colomn">
+		<div class="menu-header"></div>
+		<div class="menu"><br />
+			Login <input type="text" size="18" value="" /><br /><br />
+			Pass <input type="text" size="18" value="" /><br /><br /><input type="submit" value="OK" /><br /><br />
+			<a href="">S'enregistrer</a><br /><br />
+		</div>
+			<div class="menu-bottom"></div>
+		<div class="menu-header"></div>
+		<div class="menu"><br />
+			<b>Projets en cours</b><br /><br />
+			Conncectez-vous pour voir les projets en cours ou créer un nouveau projet.<br /><br />
+		</div>
+			<div class="menu-bottom"></div>
+	</div>
+	<div id="right-colomn">
+		<div class="menu-header"></div>
+		<div class="menu">
+		<img src="images/interface/calendrier.jpg" >
+		</div>
+			<div class="menu-bottom"></div>
+		<div class="menu-header"></div>
+		<div class="menu">
+			<br /><a href="">Forum</a><br /><br />
+		</div>
+			<div class="menu-bottom"></div>
+	</div>
+	<div id="center-colomn">
+
+
+
+		<div class="title">Projets</div>
+<div class="arborescence"><a href="../../">Projets</a> <img src="images/interface/in.png" /> <a href="../">Projet Techweb</a> <img src="images/interface/in.png" /> <a href="/">Activité 3</a></div>
+		<h3><img src="images/interface/puce.png" /> Activité 3</h3>
+		<h4><a href="">Sous-Activité 01</a> ~ <a href="">Sous-Activité 02</a></h4>
+		<p>
+		Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean condimentum eleifend justo. Ut ut purus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Duis placerat, elit in egestas cursus, purus elit tempus felis, eget suscipit sapien urna quis purus. Morbi nonummy. Ut aliquam, arcu non lacinia condimentum, lectus velit ultrices mi, in cursus orci dolor ut eros. Nam pulvinar ligula vitae tellus. Fusce erat. Curabitur dictum felis at quam. Phasellus interdum tortor in dolor. Suspendisse potenti. Suspendisse aliquet nisi vel purus.
+
+Nunc pellentesque mauris cursus felis. Donec elementum neque in est. Phasellus eros tortor, faucibus elementum, pharetra quis, porttitor et, metus. Suspendisse et ante condimentum risus tincidunt tincidunt. Donec interdum libero. Sed rutrum dolor eu velit. Maecenas ultricies, mi ac venenatis egestas, libero turpis nonummy tellus, in vulputate ipsum massa non neque. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Quisque pulvinar elementum ante. Nulla at mi eget elit pulvinar commodo. Nunc molestie blandit orci. Integer ornare dolor et lectus scelerisque aliquam. Donec congue. Proin sagittis orci.
+		</p>
+
+		<h3><img src="images/interface/puce.png" /> Enregistrement</h3>
+<div style="float:right; text-align: right; padding-right: 5px;">
+<form>
+<input type="text" value="" name="name" /><br />
+<input type="text" value="" name="pass1" /><br />
+<input type="text" value="" name="pass2" /><br />
+<input type="submit" value="S'enregistrer" name="register" /><br />
+</form>
+</div>
+<p>
+Nom<br />
+Mot de passe<br />
+Répéter le mot de passe<br /><br /><br /><br />
+</p>
+
+
+
+
+
+
+<div class="right">JumpBox <select width="100%" name="jumpbox">
+<option value="">Séléctionner</option>
+<option value="">Projets</option>
+<option value="">* Techweb</option>
+<option value="">----Activité 01</option>
+<option value="">------Sous--activité 01</option>
+<option value="">------Sous--activité 02</option>
+<option value="">----Activité 02</option>
+<option value="">------Sous--activité 01</option>
+<option value="">--------Sous--sous--activité 01</option>
+<option value="">--------Sous--sous--activité 02</option>
+<option value="">------Sous--activité 02</option>
+<option value="">----Activité 03</option>
+<option value="">------Sous--activité 01</option>
+<option value="">------Sous--activité 02</option>
+<option value="">* Philosophes</option>
+<option value="">----Activité 01</option>
+<option value="">------Sous--activité 01</option>
+<option value="">------Sous--activité 02</option>
+<option value="">----Activité 02</option>
+<option value="">------Sous--activité 01</option>
+<option value="">--------Sous--sous--activité 01</option>
+<option value="">--------Sous--sous--activité 02</option>
+<option value="">------Sous--activité 02</option>
+<option value="">----Activité 03</option>
+<option value="">------Sous--activité 01</option>
+<option value="">------Sous--activité 02</option>
+</select></div>
+
+
+	</div>
+	
+</div>
+<div id="footer"></div>
+</body>
+</html>
